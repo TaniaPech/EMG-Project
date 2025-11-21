37bdc43232faa7f49b821c784e38ca33d8b2c67b
Tania Yareni Pech Canul <141746509+TaniaPech@users.noreply.github.com>
2025-11-21 14:00:29 -0600

    Create guardarSesion.js

---
+++ api/guardarSesion.js
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby7AfORw6xkk54QBA5BBU_jeeO3rYoemSWstmVEG_OBQBFuLSZt7RKGZxHd78zTENxV/exec";
+
+export default async function handler(req, res) {
+  const { id, nombre, rms, vmax } = req.query;
+  try {
+    const url = `${GOOGLE_SCRIPT_URL}?action=guardarSesion&id=${id}&nombre=${nombre}&rms=${rms}&vmax=${vmax}`;
+    const response = await fetch(url);
+    const text = await response.text();
+    res.status(200).send(text);
+  } catch (err) {
+    res.status(500).send("Error guardando sesión");
+  }
+}
