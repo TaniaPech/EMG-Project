23c69843a7416f9c8259992b7a638a4f47c9cb52
Tania Yareni Pech Canul <141746509+TaniaPech@users.noreply.github.com>
2025-11-21 14:06:47 -0600

    Update obtenerPacientes.js

--- api/obtenerPacientes.js
+++ api/obtenerPacientes.js
@@ -1,8 +1,9 @@
 import fetch from "node-fetch";
 
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby7AfORw6xkk54QBA5BBU_jeeO3rYoemSWstmVEG_OBQBFuLSZt7RKGZxHd78zTENxV/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby7AfORw6xkk54QBA5BBU_jeeO3rYoemSWstmVEG_OBQBFuLSZxHd78zTENxV/exec";
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // para CORS
   try {
     const response = await fetch(`${GOOGLE_SCRIPT_URL}?action=obtenerPacientes`);
     const data = await response.json();
